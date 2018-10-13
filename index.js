3357ec5dbb8f92a66bead92c5cf482c3edbf21f7
Andrew Garcia <drewgarcia23@gmail.com>
2018-10-12 22:07:41 -0500

    Adding chat entry

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import RNCEChatView from './lib/RNCEChatView';
 import RNCEMessage from './lib/RNCEMessage';
 import RNCEMessageList from './lib/RNCEMessageList';
+import RNCEMessageEntry from './lib/RNCEMessageEntry';
+import RNCETypingIndicator from './lib/RNCETypingIndicator';
 
 module.exports = {
 	RNCEChatView,
 	RNCEMessage,
-	RNCEMessageList
+	RNCEMessageList,
+	RNCEMessageEntry,
+	RNCETypingIndicator
 };
